085ef5f44437395e982ce110c4bfcdd9c21d0a59
Matt Wilson <mattwwilson34@gmail.com>
2023-04-06 17:36:41 -0400

    feat(database): connect postgres db with prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,25 @@ import * as dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const products = await prisma.products.findMany({take: 1})
+  console.log(products)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
 const app = express();
 const port = 3000;
 
