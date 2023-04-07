c123f98f8147016bf02bd393a106f2e16ebb2dbd
Matt Wilson <mattwwilson34@gmail.com>
2023-04-06 20:57:49 -0400

    feat(index): add morgan, helmet, cors, and undefined route handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,31 @@
 import express from 'express';
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
 import * as dotenv from 'dotenv' 
 import path from 'path'
+import api from './api/api'
+
+// point to the .env file in the root directory
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-
-
-
-
+// create the express app
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// HTTP request logger
+app.use(morgan('dev'));
+// HTTP header security
+app.use(helmet());
+// Cross-origin resource sharing
+app.use(cors());
+// use the api router
+app.use('/api', api);
+
+// Catch-all route for 404 errors
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(() => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
