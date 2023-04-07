ccb2e34e2204d709ac13bd48cfc292d7618c6136
Matt Wilson <mattwwilson34@gmail.com>
2023-04-06 21:20:18 -0400

    feat(api): add products query to /api/products route

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get('/products', (req: Request, res: Response): void => {
-  res.send('products');
-})
+router.get('/products', async (req: Request, res: Response): Promise<void> => {
+  // query db for products limit to 10
+  const products = await prisma.products.findMany({ take: 10 });
+
+  res.send(JSON.stringify(products));
+});
 
 export default router;
