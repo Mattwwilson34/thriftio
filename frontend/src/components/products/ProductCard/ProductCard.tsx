837576fd7affaafc17b88be003dc8241e25724d2
Matt Wilson <mattwwilson34@gmail.com>
2023-04-10 13:22:53 -0400

    feat(product-card): add semantic html tags

--- frontend/src/components/products/ProductCard/ProductCard.tsx
+++ frontend/src/components/products/ProductCard/ProductCard.tsx
@@ -11,12 +11,12 @@ function ProductCard({productData}: ProductCardProps): JSX.Element {
   const {name, price, description, imageUrl} = productData
 
   return (
-    <div className={styles.productCard}>
+    <article className={styles.productCard}>
       <h1 className={styles.productName}>{name}</h1>
       <Image className={styles.productImage} src={imageUrl} alt={name} width={300} height={300} />
       <div className={styles.productPrice}>{price}</div>
       <p className={styles.productDescription}>{description}</p>
-    </div>
+    </article>
   )
 }
 
