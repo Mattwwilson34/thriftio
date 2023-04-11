2c379fa9b52f2383482755ca7a1659abe5551652
Matt Wilson <mattwwilson34@gmail.com>
2023-04-11 14:42:06 -0400

    feat(backend-api): add cursor pagination to /products endpoint

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -6,10 +6,24 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get('/products', async (req: Request, res: Response): Promise<void> => {
-  // query db for products limit to 10
-  const products = await prisma.products.findMany({ take: 10 });
+  const cursor = req.query.cursor as string | undefined;
+  const take = 10;
 
-  res.send(JSON.stringify(products));
+  const products = await prisma.products.findMany({
+    take,
+    skip: cursor ? 1 : 0,
+    cursor: cursor ? { uuid: cursor } : undefined,
+  });
+
+  const hasNextPage = await prisma.products.count({
+    where: {
+      uuid: { gt: products[products.length - 1].uuid },
+    },
+  });
+
+  const nextCursor = hasNextPage ? products[products.length - 1].uuid : null;
+
+  res.json({ products, cursor: nextCursor });
 });
 
 export default router;
