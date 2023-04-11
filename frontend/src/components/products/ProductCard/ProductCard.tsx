57c95d30816589f4f02c223b5948b21bb0f57551
Matt Wilson <mattwwilson34@gmail.com>
2023-04-11 14:43:48 -0400

    feat(product-components): add ref forwarding for pagination
    In order to implement infinit scroll a ref was needed to track
    the last product in the products list. This ref will be used in
    conjunction with an observer to fetch more products when the last
    product becomes visible in the window.:	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE

--- frontend/src/components/products/ProductCard/ProductCard.tsx
+++ frontend/src/components/products/ProductCard/ProductCard.tsx
@@ -1,23 +1,31 @@
+import { forwardRef } from 'react'
 import Image from 'next/image'
 import styles from './product-card.module.scss'
-import {Product} from '@/types/types'
+import { Product } from '@/types/types'
 
 interface ProductCardProps {
   productData: Product
 }
 
+const ProductCard = forwardRef(
+  ({ productData }: ProductCardProps, ref): JSX.Element => {
+    const { name, price, description, imageUrl } = productData
 
-function ProductCard({productData}: ProductCardProps): JSX.Element {
-  const {name, price, description, imageUrl} = productData
-
-  return (
-    <article className={styles.productCard}>
-      <h1 className={styles.productName}>{name}</h1>
-      <Image className={styles.productImage} src={imageUrl} alt={name} width={300} height={300} />
-      <div className={styles.productPrice}>{price}</div>
-      <p className={styles.productDescription}>{description}</p>
-    </article>
-  )
-}
+    return (
+      <article className={styles.productCard} ref={ref}>
+        <h1 className={styles.productName}>{name}</h1>
+        <Image
+          className={styles.productImage}
+          src={imageUrl}
+          alt={name}
+          width={300}
+          height={300}
+        />
+        <div className={styles.productPrice}>{price}</div>
+        <p className={styles.productDescription}>{description}</p>
+      </article>
+    )
+  }
+)
 
 export default ProductCard
