af2a581047dcdf4c9151a0372d05982d7f116c55
Matt Wilson <mattwwilson34@gmail.com>
2023-04-11 14:47:20 -0400

    feat(product-infinit-scroll): add infinit scroll component
    Add an infinit scroll wrapper component that works in conjunction with
    the ProductList component to fetch products form the datbase and
    when the use scrolls the last item in the current product list into
    the viewport the component will fetch another 10 products and append
    them to the list.

---
+++ frontend/src/components/products/ProductsInfinitScroll/ProductsInfinitScroll.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, useRef } from 'react'
+import type { Product } from '@/types/types'
+import { ProductList } from '@/components/products'
+
+function ProductsInfinitScroll(): JSX.Element {
+  const [products, setProducts] = useState<Product[] | []>([])
+  const [paginationCursor, setPaginationCursor] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
+  const lastProductRef = useRef<HTMLDivElement>(null)
+  const observer = useRef<IntersectionObserver | null>(null)
+
+  useEffect(() => {
+    // fetch products if there are none
+    if (products.length === 0) {
+      fetchProducts()
+    }
+
+    // remove old observer
+    if (observer.current) {
+      observer.current.disconnect()
+    }
+
+    // create new observer and observe the last product
+    if (lastProductRef.current) {
+      observer.current = new IntersectionObserver((entries) => {
+        entries.forEach(async (entry) => {
+          // if the last product is visible, fetch more products
+          if (entry.isIntersecting) {
+            await fetchProducts(paginationCursor)
+          }
+        })
+      })
+      // observe the last product
+      observer.current.observe(lastProductRef.current)
+    }
+  }, [paginationCursor])
+
+  // fetch products from the API
+  async function fetchProducts(cursor: string | null = null) {
+    const response = await fetch(`/api/products?cursor=${cursor}`)
+    const productsData = await response.json()
+    setProducts((prevProducts) => [...prevProducts, ...productsData.products])
+    setPaginationCursor(productsData.cursor)
+    setLoading(false)
+  }
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ProductList ref={lastProductRef} products={products} />
+      )}
+    </>
+  )
+}
+
+export default ProductsInfinitScroll
