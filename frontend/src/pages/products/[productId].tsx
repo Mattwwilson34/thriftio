5852acc41e2e62cef6c0ffd7e8cc70f1e3c60dff
Matt Wilson <mattwwilson34@gmail.com>
2023-04-13 13:29:45 -0400

    feat(pages): add dynamic product details page

--- frontend/src/pages/products/[productId].tsx
+++ frontend/src/pages/products/[productId].tsx
@@ -1,11 +1,54 @@
-import { useRouter } from 'next/router'
+import Image from 'next/image'
+import {Product} from '@/types/types'
+import { GetStaticPropsContext } from 'next';
 
-function ProductDetailPage() {
-  const router = useRouter()
+interface ProductDetailPageProps {
+  productData: Product
+}
 
-  const productId = router.query.productId
+function ProductDetailPage({productData}: ProductDetailPageProps) {
+  const { name, price, imageUrl } = productData
+  return (
+    <>
+      <h1>The Product Detail Page</h1>
+      <h2>{name}</h2>
+      <Image src={imageUrl} alt={name} height={100} width={100} />
+      <div>{price}</div>
+    </>
+  )
+}
 
-  return <h1>The Product Detail Page {productId}</h1>
+export async function getStaticPaths() {
+  // call API endpoint to get all product IDs
+  const response = await fetch('http://localhost:3001/api/products/productIds')
+  const productIdsStringArray = await response.json()
+
+  // create an array of paths with params (id)
+  const paths = productIdsStringArray.map((productId: string) => ({
+    params: {
+      productId: productId,
+    },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context:GetStaticPropsContext) {
+  // get product id from url params
+  const productId = context?.params?.productId
+
+  // get product data from db uding id and nextjs api
+  const productDataJson = await fetch(
+    `http://localhost:3001/api/products/${productId}`
+  )
+  // conver form json
+  const productData = await productDataJson.json()
+
+  return {
+    props: {
+      productData,
+    },
+  }
 }
 
 export default ProductDetailPage
