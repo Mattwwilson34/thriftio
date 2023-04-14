21cc75ec19d3d8caac45a8ff9a83f2269cc9a5be
Matt Wilson <mattwwilson34@gmail.com>
2023-04-14 05:29:22 -1000

    feat(pages): extract shopping cart context
    extract shopping cart context to seperate code
    concerns.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,33 +2,14 @@ import '@/styles/styles-reset.scss'
 import '@/styles/global.scss'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout'
-import React, { createContext, useReducer } from 'react'
-
-export const ShoppingCartSystem = createContext({})
-
-const initailState = {
-  cart: [],
-}
+import { ShopingCartContextProvider } from '@/context/ShoppingCartContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-  //
-  const Reducers = (state, action) => {
-    switch (action.type) {
-      case 'ADD_TO_CART':
-        const { productData } = action.payload
-        const { cart } = state
-        const newCart = [...cart, productData]
-        return { ...state, cart: newCart }
-    }
-  }
-
-  const [state, dispatch] = useReducer(Reducers, initailState)
-
   return (
-    <ShoppingCartSystem.Provider value={{ state, dispatch }}>
+    <ShopingCartContextProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ShoppingCartSystem.Provider>
+    </ShopingCartContextProvider>
   )
 }
