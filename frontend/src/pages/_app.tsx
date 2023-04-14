d55dac9f585aa4e40646ce5f75ce455b6d9d7a7b
Matt Wilson <mattwwilson34@gmail.com>
2023-04-14 04:51:54 -1000

    feat(pages): add shopping cart context and reducer
    add trial shopping cart contex direclty into the
    _app.tsx page. This context will be extracted once
    the context is confirmed to be working.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,33 @@ import '@/styles/styles-reset.scss'
 import '@/styles/global.scss'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout'
+import React, { createContext, useReducer } from 'react'
+
+export const ShoppingCartSystem = createContext({})
+
+const initailState = {
+  cart: [],
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  //
+  const Reducers = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TO_CART':
+        const { productData } = action.payload
+        const { cart } = state
+        const newCart = [...cart, productData]
+        return { ...state, cart: newCart }
+    }
+  }
+
+  const [state, dispatch] = useReducer(Reducers, initailState)
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ShoppingCartSystem.Provider value={{ state, dispatch }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ShoppingCartSystem.Provider>
   )
 }
