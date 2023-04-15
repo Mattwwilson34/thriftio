0304892063d332d28a4a5b222e60911bdb23ffd9
Matt Wilson <mattwwilson34@gmail.com>
2023-04-15 07:41:19 -1000

    feat(nav): add background color to cart number

--- frontend/src/components/cart/Cart/Cart.tsx
+++ frontend/src/components/cart/Cart/Cart.tsx
@@ -1,12 +1,22 @@
+import { useContext } from 'react'
 import styles from './cart.module.scss'
 import Image from 'next/image'
 import shoppingCartIcon from '../../../../public/icons/shopping-cart.svg'
+import { ShoppingCartContext } from '@/context/ShoppingCartContext'
 
 function Cart() {
+  const { state } = useContext(ShoppingCartContext)
+  const { cart } = state
+  const numberOfItemsInCart = cart.length
+
   return (
     <>
-      <Image src={shoppingCartIcon} alt="Shopping cart icon" className={styles.shoppingCartIcon} />
-      <span className={styles.shoppingCartNumber}>0</span>
+      <Image
+        src={shoppingCartIcon}
+        alt="Shopping cart icon"
+        className={styles.shoppingCartIcon}
+      />
+      <span className={styles.shoppingCartNumber}>{numberOfItemsInCart}</span>
       <span className={styles.shoppingCartLabel}>cart</span>
     </>
   )
