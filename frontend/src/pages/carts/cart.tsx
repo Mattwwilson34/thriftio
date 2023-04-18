bc2a1a521edd596e3987828a5232a2619736b5c9
Matt Wilson <mattwwilson34@gmail.com>
2023-04-18 11:24:40 -1000

    feat(cart-page): add ShoppingCartContext consumption

--- frontend/src/pages/carts/cart.tsx
+++ frontend/src/pages/carts/cart.tsx
@@ -1,9 +1,16 @@
-function CartPage() {
+import { useContext } from 'react'
+import { ShoppingCartContext } from '@/context/ShoppingCartContext'
+
+function CartPage() { 
+  const { state } = useContext(ShoppingCartContext)
+  const { cart } = state
+  console.log(cart)
+
   return (
     <div>
       <h1>Cart</h1>
     </div>
-  );
+  )
 }
 
-export default CartPage;
+export default CartPage
