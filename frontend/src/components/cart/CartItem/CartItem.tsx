1e7b9381c224abda82a61c31fad16aa7bca0d418
Matt Wilson <mattwwilson34@gmail.com>
2023-04-25 20:16:01 -0400

    feat(CartItem): add html elements for product info

--- frontend/src/components/cart/CartItem/CartItem.tsx
+++ frontend/src/components/cart/CartItem/CartItem.tsx
@@ -1,4 +1,5 @@
 import styles from './cart-item.module.scss'
+import Link from 'next/link'
 import Image from 'next/image'
 import { Product } from '@/types/types'
 
@@ -7,12 +8,27 @@ interface CartItemProps {
 }
 
 function CartItem({ product }: CartItemProps) {
-  const { name, price, imageUrl} = product
+  const { name, price, imageUrl } = product
   return (
     <li className={styles.cartItem}>
-      <input  type="checkbox" />
+      <input type="checkbox" />
       <Image src={imageUrl} alt={name} width={100} height={100} />
-      <h1>{name}</h1>
+      <div className={styles.cartItemInfoContainer}>
+        <h1>{name}</h1>
+        <span>In Stock</span>
+        <div className={styles.cartItemInputContainer}>
+          <span> Qty: </span>
+          <input
+            className={styles.cartItemQuantity}
+            type="number"
+            name="cartItemQuantity"
+            min="0"
+            max="100"
+            defaultValue="1"
+          />
+          <Link href="/">Delete</Link>
+        </div>
+      </div>
       <span>{price}</span>
     </li>
   )
