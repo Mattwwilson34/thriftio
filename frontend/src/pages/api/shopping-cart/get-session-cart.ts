b62a5a023e9286114d91bd606665017f988e249a
Matt Wilson <mattwwilson34@gmail.com>
2023-04-30 20:11:00 -0400

    feat(frontend-api): add /get-sesion/cart route

---
+++ frontend/src/pages/api/shopping-cart/get-session-cart.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// create a type for the error response
+type ErrorResponse = {
+  error: {
+    message: string
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    console.log('line:17:', req.cookies)
+
+    const response = await fetch(
+      'http://localhost:3000/api/cart',
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Cookie': req.headers.cookie ?? '',
+        },
+      }
+    )
+
+    const shoppingCartSessionData = await response.json()
+
+    console.log('line:32:', shoppingCartSessionData)
+    res.send(JSON.stringify(shoppingCartSessionData))
+
+
+  } catch (error: any) {
+    const errorResponse: ErrorResponse = {
+      error: {
+        message: error.message,
+      },
+    }
+    // if there is an error, return the error message
+    res.status(500).json(errorResponse)
+  }
+}
