29ba78115428cf0af92dd414f4cb2615694751bf
Matt Wilson <mattwwilson34@gmail.com>
2023-05-05 18:02:56 -0400

    feat(nav): wrap logo in link that takes user to home page and add styles

--- frontend/src/components/nav/Nav/Nav.tsx
+++ frontend/src/components/nav/Nav/Nav.tsx
@@ -6,7 +6,9 @@ function Nav(): JSX.Element {
     <nav className={styles.nav}>
       <header>
         <h1 className={styles.header}>
+          <a href="http://localhost:3001">
           thrift<span className={styles.headerInterpunct}>.</span>io
+          </a>
         </h1>
       </header>
       <div className={styles.cartContainer}>
