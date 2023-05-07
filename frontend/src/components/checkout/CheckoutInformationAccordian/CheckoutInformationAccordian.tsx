e4fdf34eef3cdf0a1e2efdcc4efd72ea0e6884d6
Matt Wilson <mattwwilson34@gmail.com>
2023-05-07 18:35:09 -0400

    feat(checkout-components): add address form state

--- frontend/src/components/checkout/CheckoutInformationAccordian/CheckoutInformationAccordian.tsx
+++ frontend/src/components/checkout/CheckoutInformationAccordian/CheckoutInformationAccordian.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import styles from './checkout-information-accordian.module.scss'
 import CheckoutShippingAddressForm from '../CheckoutShippingAddressForm/CheckoutShippingAddressForm'
 
 function CheckoutInformationAccordian() {
+  // shipping address form state
+  const [shippingAddressFormState, setShippingAddressFormState] = useState({
+    name: '',
+    streetAddressLineOne: '',
+    streetAddressLineTwo: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    country: '',
+    phone: '',
+  })
+
   return (
     <div className={styles.checkoutInformationAccordian}>
-      <CheckoutShippingAddressForm />
+      <CheckoutShippingAddressForm
+        shippingAddressFormState={shippingAddressFormState}
+        setShippingAddressFormState={setShippingAddressFormState}
+      />
     </div>
   )
 }
--- frontend/src/components/checkout/CheckoutShippingAddressForm/CheckoutShippingAddressForm.tsx
+++ frontend/src/components/checkout/CheckoutShippingAddressForm/CheckoutShippingAddressForm.tsx
@@ -1,38 +1,115 @@
+import { useState } from 'react'
 import styles from './checkout-shipping-address-form.module.scss'
 import states from '@/utils/form-dropdown-data/us-states.json'
 import { countries } from '@/utils/form-dropdown-data/countries'
 
-function CheckoutShippingAddressForm() {
+type ShippingAddressFormState = {
+  name: string
+  streetAddressLineOne: string
+  streetAddressLineTwo: string
+  city: string
+  state: string
+  zipCode: string
+  country: string
+  phone: string
+}
+
+type CheckoutShippingAddressFormProps = {
+  shippingAddressFormState: ShippingAddressFormState
+  setShippingAddressFormState: React.Dispatch<
+    React.SetStateAction<ShippingAddressFormState>
+  >
+}
+
+function CheckoutShippingAddressForm({
+  shippingAddressFormState: formState,
+  setShippingAddressFormState,
+}: CheckoutShippingAddressFormProps) {
+  //
+  // handle input change
+  function handleInputChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const { id, value } = e.target
+    if (e.target.tagName === 'SELECT') {
+      // Handle select elements separately
+      setShippingAddressFormState((prevState) => ({
+        ...prevState,
+        [id]: value,
+      }))
+    } else {
+      // Handle input elements
+      setShippingAddressFormState((prevState) => ({
+        ...prevState,
+        [id]: value,
+      }))
+    }
+  }
+
+  // handle form submission
+  function handleFormSubmission(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(formState)
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleFormSubmission}>
+      {/* first and last name input */}
       <label htmlFor="name">
         Full Name (First and Last name)
-        <input id="name" type="text" required></input>
+        <input
+          id="name"
+          type="text"
+          value={formState.name}
+          onChange={handleInputChange}
+          required
+        ></input>
       </label>
+      {/* street address line 1 */}
       <label htmlFor="streetAddressLineOne">
         Address
         <input
           id="streetAddressLineOne"
           type="text"
+          value={formState.streetAddressLineOne}
           placeholder="Street address or P.O Box"
+          onChange={handleInputChange}
           required
         ></input>
       </label>
+      {/* street address line 2 */}
       <label htmlFor="streetAddressLineTwo">
         Address
         <input
           id="streetAddressLineTwo"
           type="text"
+          value={formState.streetAddressLineTwo}
+          onChange={handleInputChange}
           placeholder="Apt, suite, unit, building, floor, etc."
         ></input>
       </label>
+      {/* city input */}
       <label htmlFor="city">
         City
-        <input id="city" type="text" required></input>
+        <input
+          id="city"
+          type="text"
+          value={formState.city}
+          onChange={handleInputChange}
+          required
+        ></input>
       </label>
+      {/* state input */}
       <label htmlFor="state">
         State
-        <select id="state" required>
+        <select
+          id="state"
+          value={formState.state}
+          onChange={handleInputChange}
+          required
+        >
           <option value="" disabled>
             Select
           </option>
@@ -41,19 +118,28 @@ function CheckoutShippingAddressForm() {
           ))}
         </select>
       </label>
+      {/* zip code input */}
       <label htmlFor="zipCode">
         Zip Code
         <input
           id="zipCode"
           type="number"
+          value={formState.zipCode}
           pattern="[0-9]{5}"
           placeholder="5-digit zip code"
+          onChange={handleInputChange}
           required
         ></input>
       </label>
+      {/* country input */}
       <label htmlFor="country">
         Country
-        <select id="country" required>
+        <select
+          id="country"
+          value={formState.country}
+          onChange={handleInputChange}
+          required
+        >
           <option value="" disabled>
             Select
           </option>
@@ -62,17 +148,20 @@ function CheckoutShippingAddressForm() {
           ))}
         </select>
       </label>
+      {/* phone number input */}
       <label htmlFor="phone">
         Phone Number
         <input
           id="phone"
           type="tel"
+          value={formState.phone}
           pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
           placeholder="999-999-9999"
+          onChange={handleInputChange}
           required
         ></input>
       </label>
-
+      {/* submit button */}
       <label htmlFor="continueToPayment">
         <input
           id="continueToPayment"
