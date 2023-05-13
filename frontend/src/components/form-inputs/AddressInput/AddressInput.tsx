6b6492065b842ec95e3c7270ca0a14a461e00da5
Matt Wilson <mattwwilson34@gmail.com>
2023-05-13 17:47:08 -0400

    feat(form-inputs): add AddressInput component

--- frontend/src/components/form-inputs/AddressInput/AddressInput.tsx
+++ frontend/src/components/form-inputs/AddressInput/AddressInput.tsx
@@ -1,24 +1,24 @@
 interface Props {
-  address: string;
-  setAddress: (address: string) => void;
-  placeholder?: string;
-  required?: boolean;
-  label?: string;
-}
-function AddressInput({ address, setAddress, placeholder, required, label }: Props) {
-  return (
-      <label htmlFor="address">
-        {label ? label : ''}
-        <input
-          id="address"
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder={placeholder ? placeholder : ''}
-          required={required ? required : false}
-        ></input>
-      </label>
-  );
+  address: string
+  setAddress: (address: string) => void
+  placeholder?: string
+  required?: boolean
 }
 
-export default AddressInput;
+function AddressInput({ address, setAddress, placeholder, required }: Props) {
+  return (
+    <label htmlFor="address">
+      Street Address
+      <input
+        id="address"
+        type="text"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        placeholder={placeholder ? placeholder : ''}
+        required={required ? required : false}
+      ></input>
+    </label>
+  )
+}
+
+export default AddressInput
