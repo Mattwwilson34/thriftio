47416634f329c56924a3e7f67a9d2e45fb9e126b
Matt Wilson <mattwwilson34@gmail.com>
2023-05-17 12:39:35 -0400

    feat(checkout-page): add buttons for redirect to '/' + '/checkout'

--- frontend/src/pages/carts/cart.tsx
+++ frontend/src/pages/carts/cart.tsx
@@ -1,21 +1,31 @@
 import { useContext } from 'react'
+import { useRouter } from 'next/router'
 import { ShoppingCartContext } from '@/context/ShoppingCartContext'
-import {CartItems, CartTotal} from '@/components/cart/'
+import { CartItems, CartTotal } from '@/components/cart/'
 import styles from './cart.module.scss'
 
-function CartPage() { 
+function CartPage() {
+  const router = useRouter()
   const { state } = useContext(ShoppingCartContext)
   const { shoppingCart } = state
 
   const cartCount = shoppingCart.length
+
+  // redirect to home page on button click
+  const handleContinueShopping = () => router.push('/')
+
+  // redirect to checkout on button click
+  const handleCheckout = () => router.push('/checkout/checkout')
+
   return (
     <div className={styles.cartContainer}>
-      <CartItems cartItems={shoppingCart} />
-      <CartTotal cartItems={shoppingCart} />
       {/* empty cart message */}
       {cartCount === 0 && (
         <>
           <h1 className={styles.emptyCart}>Your cart is empty.</h1>
+          <button className={styles.continueShoppingBtn} onClick={handleContinueShopping}>
+            Continue Shopping
+          </button>
         </>
       )}
       {/* cart not empty display cart */}
@@ -23,6 +33,7 @@ function CartPage() {
         <>
           <CartItems cartItems={shoppingCart} />
           <CartTotal cartItems={shoppingCart} />
+          <button className={styles.checkoutBtn} onClick={handleCheckout}>Checkout</button>
         </>
       )}
     </div>
