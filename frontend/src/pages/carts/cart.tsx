16c61073dd39a92247fa48c85fdf2706a3563302
Matt Wilson <mattwwilson34@gmail.com>
2023-05-17 12:37:41 -0400

    feat(cart-page): conditionaly render empty cart message

--- frontend/src/pages/carts/cart.tsx
+++ frontend/src/pages/carts/cart.tsx
@@ -7,10 +7,24 @@ function CartPage() {
   const { state } = useContext(ShoppingCartContext)
   const { shoppingCart } = state
 
+  const cartCount = shoppingCart.length
   return (
     <div className={styles.cartContainer}>
       <CartItems cartItems={shoppingCart} />
       <CartTotal cartItems={shoppingCart} />
+      {/* empty cart message */}
+      {cartCount === 0 && (
+        <>
+          <h1 className={styles.emptyCart}>Your cart is empty.</h1>
+        </>
+      )}
+      {/* cart not empty display cart */}
+      {cartCount > 0 && (
+        <>
+          <CartItems cartItems={shoppingCart} />
+          <CartTotal cartItems={shoppingCart} />
+        </>
+      )}
     </div>
   )
 }
