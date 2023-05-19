dc21cc58182d1d52818b38620bc10466b2b17e9d
Matt Wilson <mattwwilson34@gmail.com>
2023-05-19 09:44:08 -0400

    feat(search-bar): add debounce to text input

--- frontend/src/components/search-bar/SearchBar/SearchBar.tsx
+++ frontend/src/components/search-bar/SearchBar/SearchBar.tsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react'
 import styles from './search-bar.module.scss'
 import Image from 'next/image'
 import searchIcon from '../../../../public/icons/search-icon.svg'
 
 function SearchBar() {
+  const [searchText, setSearchText] = useState<string>('')
+
+  useEffect(() => {
+    // setup debounce for search
+    const delay = 500
+    const debounceTimer = setTimeout(() => {
+      console.log(`Search for ${searchText}`)
+    }, delay)
+
+    return () => {
+      clearTimeout(debounceTimer)
+    }
+    
+  },[searchText])
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    // update text when input changes
+    const { value } = e.target
+    setSearchText(value)
+
+  }
+
   return (
     <div className={styles.searchBar}>
       <label htmlFor="searchInput" className={styles.searchLabel}>
@@ -10,6 +33,8 @@ function SearchBar() {
           className={styles.searchInput}
           type="text"
           placeholder="Search thrift.io"
+          onChange={handleChange}
+          value={searchText}
         />
       </label>
       <label htmlFor="categorySelector" className={styles.categoryLabel}>
@@ -17,8 +42,7 @@ function SearchBar() {
           <option value="All Categories">All Categories</option>
         </select>
       </label>
-        <div className={styles.spacer}>
-        </div>
+      <div className={styles.spacer}></div>
       <div className={styles.searchButton}>
         <Image src={searchIcon} alt="search-icon" />
       </div>
