0247979395dcf5461ef34f4efc02c261bce23a7f
Matt Wilson <mattwwilson34@gmail.com>
2023-05-19 13:47:10 -0400

    feat(search-bar): connect db query to SeachBar
    Added logic to hit backend api with text based
    search query. The response is used to create a dynamic
    search suggestion list.

--- frontend/src/components/search-bar/SearchBar/SearchBar.tsx
+++ frontend/src/components/search-bar/SearchBar/SearchBar.tsx
@@ -3,49 +3,91 @@ import styles from './search-bar.module.scss'
 import Image from 'next/image'
 import searchIcon from '../../../../public/icons/search-icon.svg'
 
+interface SearchResults {
+  uuid: string
+  name: string
+}
+
 function SearchBar() {
   const [searchText, setSearchText] = useState<string>('')
+  const [searchResults, setSearchResults] = useState<SearchResults[]>([])
 
   useEffect(() => {
+    // databse query function
+    async function dbProductQuery(searchText: string) {
+      const queryURL = `http://localhost:3000/api/product-search?search=${searchText}`
+      try {
+        // include credentials to send cookies
+        const response = await fetch(queryURL, {
+          credentials: 'include',
+        })
+        const productData = await response.json()
+
+        setSearchResults(productData)
+      } catch (err) {
+        if (err) console.log(err)
+      }
+    }
+
     // setup debounce for search
     const delay = 500
     const debounceTimer = setTimeout(() => {
+      // handle empty search
+      if (searchText === '') return
+      // handle search
       console.log(`Search for ${searchText}`)
+      dbProductQuery(searchText)
     }, delay)
 
     return () => {
       clearTimeout(debounceTimer)
     }
-    
-  },[searchText])
+  }, [searchText])
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     // update text when input changes
     const { value } = e.target
     setSearchText(value)
-
   }
 
+  // search results boolean for displaying search suggestions
+  const hasSearchResults = searchResults.length > 0 && searchText !== ''
+
   return (
-    <div className={styles.searchBar}>
-      <label htmlFor="searchInput" className={styles.searchLabel}>
-        <input
-          className={styles.searchInput}
-          type="text"
-          placeholder="Search thrift.io"
-          onChange={handleChange}
-          value={searchText}
-        />
-      </label>
-      <label htmlFor="categorySelector" className={styles.categoryLabel}>
-        <select className={styles.categorySelector}>
-          <option value="All Categories">All Categories</option>
-        </select>
-      </label>
-      <div className={styles.spacer}></div>
-      <div className={styles.searchButton}>
-        <Image src={searchIcon} alt="search-icon" />
+    <div className={styles.searchContainer}>
+      <div className={styles.searchBar}>
+        <label htmlFor="searchInput" className={styles.searchLabel}>
+          <input
+            className={styles.searchInput}
+            type="text"
+            placeholder="Search thrift.io"
+            onChange={handleChange}
+            value={searchText}
+          />
+        </label>
+        <label htmlFor="categorySelector" className={styles.categoryLabel}>
+          <select className={styles.categorySelector}>
+            <option value="All Categories">All Categories</option>
+          </select>
+        </label>
+        <div className={styles.spacer}></div>
+        <div className={styles.searchButton}>
+          <Image src={searchIcon} alt="search-icon" />
+        </div>
       </div>
+      {hasSearchResults && (
+        <div className={styles.searchSuggestionsContainer}>
+          <ul className={styles.searchSuggestions}>
+            {searchResults.map((result) => {
+              return (
+                <li className={styles.searchResultListItem} key={result.uuid}>
+                  {result.name}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
